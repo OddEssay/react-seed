b0e2f6bb9654261ef5217b54b6bd25d58d4df13c
Paul Bennett-Freeman <clicknmix@gmail.com>
2016-09-10 13:52:44 +0100

    Move __tests__ dir under the related component folder

--- src/components/__tests__/HelloMessage.test.js
+++ src/components/__tests__/HelloMessage.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { HelloMessage } from '../src/components/HelloMessage'
+import { HelloMessage } from '../HelloMessage'
 test('HelloMessage', () => {
   const tree = renderer.create(
     <HelloMessage params={ {name: 'World'} } />
