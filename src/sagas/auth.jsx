f1ba43a7f71b36903ac977ed24fad25886a2b810
Paul Bennett-Freeman <clicknmix@gmail.com>
2016-09-12 12:17:13 +0100

    Use react-router-redux as middleware to redirect in sagas
      * The `push` action provided by react-router-redux makes it simple
        to redirect the user to a new URL
      * In `src/store.jsx` we add the routerMiddleware so this works

--- src/sagas/auth.jsx
+++ src/sagas/auth.jsx
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import firebase from 'firebase'
 import { call, put } from 'redux-saga/effects'
 import { takeEvery } from 'redux-saga'
+import { push } from 'react-router-redux'
 
 function* registerAuth(action){
   try {
@@ -11,6 +12,7 @@ function* registerAuth(action){
 
     const register = yield call(doRegister)
     yield put({ type: 'AUTH_REGISTER_SUCCEEDED', result: register })
+    yield put( push('/success') )
   } catch(e) {
     yield put({ type: 'AUTH_REGISTER_FAILED', message: e.message })
   }
--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,8 @@
 import 'babel-polyfill'
 import { createStore, compose, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { routerMiddleware } from 'react-router-redux'
+import { browserHistory } from 'react-router'
 
 import rootReducer from './reducers'
 import authSaga from './sagas/auth'
@@ -9,8 +11,10 @@ const initialState = {}
 
 const sagaMiddleware = createSagaMiddleware()
 
+const historyMiddleware = routerMiddleware(browserHistory)
+
 const middleware = compose(
-  applyMiddleware(sagaMiddleware),
+  applyMiddleware(sagaMiddleware, historyMiddleware),
   window.devToolsExtension && window.devToolsExtension()
 )
 
