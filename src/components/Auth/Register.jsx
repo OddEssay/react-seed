664dead51570468d145138d5432724a426faf783
Paul Bennett-Freeman <clicknmix@gmail.com>
2016-09-12 10:56:31 +0100

    Bind a form to simple register action in Register component

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import autoBind from 'react-autobind'
 import { connect } from 'react-redux'
-
+import { bindActionCreators } from 'redux'
 import * as authActions from '../../actions/auth'
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    authActions,
+    authActions: bindActionCreators(authActions, dispatch),
   }
 }
 export class Register extends React.Component {
@@ -16,18 +16,54 @@ export class Register extends React.Component {
     authActions: React.PropTypes.object.isRequired,
   }
 
-  static defaultProps = {
-  }
-
   constructor(props) {
     super(props)
     autoBind(this)
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleChange(field, event) {
+    const value = event.target.value
+    this.setState( { [ field ]: value } )
+  }
+
+  handleRegister() {
+    const { email, password } = this.state
+    this.props.authActions.register( email, password )
   }
 
   render(){
     return (
       <div>
         Register
+        <form>
+          <label>Email</label>
+          <input
+            type='text'
+            onChange={ event => this.handleChange('email', event) }
+          />
+
+          <br />
+
+          <label>Password</label>
+          <input
+            type='password'
+            onChange={ event => this.handleChange('password', event) }
+          />
+
+          <br />
+
+          <input
+            type='button'
+            onClick={this.handleRegister}
+            value='Register'
+          />
+
+        </form>
       </div>
     )
   }
